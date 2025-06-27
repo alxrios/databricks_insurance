b78ccf7c6b2920e0f1b3ca21f51f323686b3c71c
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2025-06-27 22:00:00 +0200

    Update databricks_insuranceSCRIPT.R

--- databricks_insuranceSCRIPT.R
+++ databricks_insuranceSCRIPT.R
@@ -2,9 +2,12 @@
 # This script is intended for helping with the elaboration of the document databricks_insuranceRMKD. #
 ######################################################################################################
 
+# TO DO: Explore the repeated policy numbers in the dataset claims
+
 # Necessary libraries
 library(ggplot2)
 library(jsonlite)
+library(data.table)
 
 # Data load
 policies <- read.csv("https://raw.githubusercontent.com/databricks-industry-solutions/dlt-insurance-claims/refs/heads/main/data/samples/mysql/policies.csv")
@@ -22,135 +25,257 @@ names(claims)
 length(unique(claims$policy_no))
 length(unique(policies$POLICY_NO))
 
-# Duplicate values should be removed?
+# More repeated values in the dataset claims?
+head(sort(table(claims$policy_no), decreasing = T))
 
-# Let's start with only one case
-which(claims$policy_no[1] == policies$POLICY_NO)
+# Is the information in all the rows with the repeated policy numbers equal or different?
+# Let's check first the case with more observations, the policy number: 102122085
 
-# Let's create a dataframe with one column with the policy numbers contained in the dataset claims
-# and another column with the indexes in which they appear in the dataset policies
+claims[which(claims$policy_no == "102122085"), ]
 
-checkCoincide <- data.frame(pol_n = character(), policies_index = numeric())
+# Is this policy number present in the other dataset?
 
-getIndexc <- function(pol_num) {
-  # Returns the index or indexes in policies which policy number coincides with the one
-  # passed in the parameter pol_num.
-  # pol_num must be a character single element.
-  # Return is an vector integer of variable length.
-  # Function parameter is assumed to be passed correctly by the user
-  return(which(pol_num == policies$POLICY_NO))
+which(policies$POLICY_NO == "102122085")
+
+# Yes, only one appearance.
+
+policies[which(policies$POLICY_NO == "102122085"), ]
+
+# Let's inspect another repeated values
+
+claims[which(claims$policy_no == "101511451"), ]
+
+# Is this policy number present in the policies dataset?
+policies[which(policies$POLICY_NO == "101511451"), ]
+
+# Since it seems to be registered data in claims dataset with posterior dates to the 
+# information obtained in the policies dataset, let's only use the observations which
+# policy number appears only once in the claims dataset.
+
+length(unique(policies$POLICY_NO))
+length(policies$POLICY_NO) - length(unique(policies$POLICY_NO))
+
+# In the policies dataset are also 21 repeated values, let's check if they take the same values
+# for each variable.
+
+head(sort(table(policies$POLICY_NO), decreasing = TRUE), 21)
+
+# Checking 101610176/1
+
+policies[which(policies$POLICY_NO == "101610176/1"), ]
+# Discrepancies seen in columns BOROUGH, NEIGHBORHOOD, ZIP_CODE, PREMIUM and DEDUCTABLE.
+
+policies[which(policies$POLICY_NO == "102000969/RI"), ]
+# Discrepancies found in some columns.
+
+policies[which(policies$POLICY_NO == "102065942"), ]
+# Only discrepancies found in the columns NEIGHBORHOOD, ZIP_CODE and DEDUCTABLE.
+
+policies[which(policies$POLICY_NO == "102098939"), ]
+# Discrepancies in NEIGHBORHOOD, ZIP_CODE and DEDUCTABLE
+
+policies[which(policies$POLICY_NO == "102107577"), ]
+# Discrepancies found in ZIP_CODE
+
+policies[which(policies$POLICY_NO == "102120560"), ]
+# Discrepancies in DRV_DOB, ZIP_CODE, PRODUCT and PREMIUM.
+
+policies[which(policies$POLICY_NO == "102134102"), ]
+# Discrepancies found in BODY, DRV_DOB, ZIP_CODE and DEDUCTABLE.
+
+# 15 observations has no policy number:
+policies[which(policies$POLICY_NO == ""), ]
+
+# Let's try to obtain this information with a single function.
+
+duplicated_policies <- names(head(sort(table(policies$POLICY_NO), decreasing = TRUE), 8)[-1])
+
+na_to_char <- function(input_vector) {
+  # This function recieves a row of a data.frame object and if it contains NA values, it returns
+  # the same vector with the NA's also as characters.
+  # Input by the user is assumed to be correct.
+  if (length(input_vector[which(is.na(input_vector))]) > 0) {
+    input_vector[which(is.na(input_vector))] <- "NA"
+  }
+  return(input_vector)
 }
 
-# test
-getIndexc(claims$policy_no[1])
-getIndexc("333")
-
-length(sapply(claims$policy_no[1:5], getIndexc))
-sapply(claims$policy_no[1:5], getIndexc)
-# How big will be the resulting dataframe?
-length(sapply(claims$policy_no, getIndexc))
-# Same length than the claims dataset so maybe repeated values are the ones in claims, 
-# let's continue checking it.
-indexes <- vapply(claims$policy_no, getIndexc, integer(1))
-# Error, all results weren't of length one
-#
-# Let's try again with a loop
-for (i in claims$policy_no) {
-  indexes <- getIndexc(i)
-  if (length(indexes > 0)) {
-    for (k in indexes) {
-      checkCoincide[dim(checkCoincide)[1] + 1,] <- c(i, k)
+checkDiscrepancies <- function(p_numbers) {
+  # Input: p_numbers, a character vector with the policy numbers to check.
+  # If both rows don't have the same values for each column, a data.frame object
+  # with the content of the columns that doesn't match is printed in the console.
+  for (i in p_numbers) {
+    check <- policies[which(policies$POLICY_NO == i), ]
+    check[1, ] <- na_to_char(check[1, ])
+    check[2, ] <- na_to_char(check[2, ])
+    discrepancies <- which(check[1, ] != check[2, ])
+    if (length(discrepancies) > 0) {
+      cat("Discrepancies for the policy number ", i, " between the two rows found at the variables:\n")
+      print("---------------------------------------------------------------------------------------")
+      auxiliar_frame <- data.frame(variable_name = colnames(check[discrepancies]), 
+                                   row_one_values = unlist(check[1, discrepancies]), 
+                                   row_two_values = unlist(check[2, discrepancies]))
+      print(auxiliar_frame)
+      print("---------------------------------------------------------------------------------------")
+    } else {
+      cat("No discrepancies were found for the policy with the number: ", i)
     }
   }
 }
 
-# Testing how to proceed inside the loop when getIndexc returns more than one observation
-pol <- claims$policy_no[1] # example policy
-indexes <- round(runif(5, 10000, 20000)) # example indexes
-for (i in indexes) {
-  checkCoincide[(dim(checkCoincide)[1] + 1),] <- c(pol, i)
+checkDiscrepancies(duplicated_policies)
+
+# Are this duplicated policy numbers present in the dataset claims?
+# Yes, the dataset claims contain observations for some of this repeated policies.
+# Conclussion is to drop them before the merge, also with the duplicates present in the dataset
+# claims.
+
+# Let's remove now the repeated policies from both datasets.
+# We don't want to conserve the unique ones, but remove all the repeated ones.
+# So this code isn't value: 
+# unique_pol_n <- unique(claims$policy_no)
+# because it conserves one observation of for each repeated policy.
+
+# There are policy numbers from the ducplicated ones in the dataset policies in this vector?
+
+for (i in duplicated_policies) {
+  print(which(unique_pol_n == i))
 }
-# Maybe is better idea to make an auxiliar dataframe inside the loop, this way
-# the indexes column remains being numeric
 
-####################################################################
-# Note, let's change to data.table to see if the process is faster #
-####################################################################
+# Let's store first the policy numbers with repetitions
 
-length(unique(checkCoincide$pol_n))
-length(unique(checkCoincide$policies_index))
-50642 - 45342
-50642 - 45344
+duplicated_pol_claims <- sort(table(claims$policy_no), decreasing = T)
+length(duplicated_pol_claims[which(duplicated_pol_claims > 1)])
+# So 4928 observations should be removed from this dataset.
+duplicated_pol_claims <- duplicated_pol_claims[which(duplicated_pol_claims > 1)]
+duplicated_pol_claims <- names(duplicated_pol_claims)
 
-# There are 2 less observations in the unique of the first column, so one policy would appear
-# in two different rows in policies, so if we are going to use only the unique values of policy
-# numbers for example this two should be eleminated also.
+# We need to store the row indexes of the observations to drop from the dataset.
+drop_index <- numeric()
+for (i in duplicated_pol_claims) {
+  drop_index <- c(drop_index, which(claims$policy_no == i))
+}
 
-# Let's check now if all repeated policy_numbers in checkCoincide take the same index in the policies
-# data.frame.
+# Also is necessary to remove the repeated policies observed in the dataset policies.
 
-freqPlusOne <- sort(table(checkCoincide$pol_n), decreasing = T)
-# Subsample of freqPlusOne, only policy numbers which frequency is bigger than 1
-freqPlusOne2 <- data.frame(pol_n = names(freqPlusOne[which(freqPlusOne > 1)]), 
-                           frequency = freqPlusOne[which(freqPlusOne > 1)])
-# The previous one repeated the column with the names
-freqPlusOne2 <- data.frame("frequencies" = freqPlusOne[which(freqPlusOne > 1)])
-# Rename the columns
-names(freqPlusOne2) <- c("pol_n", "frequencies")
-# Now we have to check if for all this policy numbers, the indexes in checkCoincide (second column)
-# are the same
+test <- numeric()
+for (i in duplicated_policies) {
+  test <- c(test, which(claims$policy_no == i))
+}
 
-# For that purpose let's create a new function and use vapply
-checkIndex <- function(pol_num) {
-  # Returns 1 if the length obtained when searching for the corresponding indexes to the
-  # policy number passed as paremeter in pol_num is one and 0 otherwise.
-  result <- 0 # zero by the default
-  if (length(unique(checkCoincide$policies_index[which(checkCoincide$pol_n == pol_num)])) > 1) {
-    result <- 1
+print(test)
+# Are this possitions already in the drop_index vector?
+which(drop_index == test[1])
+which(drop_index == test[2])
+# Ans: no
+
+# So let's add them
+for (i in duplicated_policies) {
+  drop_index <- c(drop_index, which(claims$policy_no == i))
+}
+
+# Can they be removed using only the minus sign?
+
+dim(claims)
+dim(claims[-drop_index, ])
+
+# Before the merge, let's check how many policies of the "after drop" claims dataset are
+# present in the policies dataset, id est, let's check how long will be the resulting dataset.
+
+if (length(integer(0))) {
+  print("Yes")
+} else {
+  print("No")
+}
+
+if (length(1888)) {
+  print("yes")
+} else {
+  print("no")
+}
+
+future_length <- 0
+for (i in claims[-drop_index, "policy_no"]) {
+  check <- which(policies$POLICY_NO == i)
+  if (length(check)) {
+    future_length <- future_length + 1
   }
-  return(result)
 }
 
-sum(vapply(freqPlusOne2$pol_n, checkIndex, numeric(1)))
-# sum is 2, so yes there are 2 repeated indexes in policies (?), so one same policy at claims
-# appears in two different row of policies. Let's identify them.
-getIndex <- function(pol_num) {
-  # Returns c(-1, -1) if the corresponding index to the policy number given as parameter to pol_num
-  # are more than one in the dataset checkCoincide and return the indexes otherwise.
-  result <- -1
-  check <- unique(checkCoincide$policies_index[which(checkCoincide$pol_n == pol_num)])
-  if (length(check) > 1 ) {
-    result <- as.numeric(check)
-  }
-  return(result)
-}
+print(future_length)
+print(dim(claims[-drop_index, ]))
 
-tst <- sapply(freqPlusOne2$pol_n, getIndex)
-unlist(tst)[which(unlist(tst) != -1)]
-# Ans: 164824 165589 187826 187827, let's observe their policy numbers
-checkCoincide[which(checkCoincide$policies_index == "164824")]
-checkCoincide[which(checkCoincide$policies_index == "165589")]
-checkCoincide[which(checkCoincide$policies_index == "187826")]
-checkCoincide[which(checkCoincide$policies_index == "187827")]
+# Length will be two observations less than the one of the claims dataset after 
+# dropping out the duplicates.
 
-# Next let's check the values of the other variables for this observations in the dataset
-# policies and claims. Are the same or different?
+# Let's go with the merge
+insurance <- merge(claims[-drop_index, ], policies, by.x = c("policy_no"), 
+                    by.y = c("POLICY_NO"), all.x = T)
 
-policies$POLICY_NO[164824]
-policies$POLICY_NO[165589]
-policies$CUST_ID[c(164824, 165589)]
-policies$POLICYTYPE[c(164824, 165589)]
+# Let's check if we have the predicted length for the rows
+dim(insurance)
+# It looks that there are two extra rows, let's explore if this is caused because of observations
+# with NA values.
+which(is.na(insurance$policy_no))
+# Yes, there are two observations with NA as their policy number. 
+print(insurance[which(is.na(insurance$policy_no)), ])
+# Since there are NA values in almost every column for this two rows, let's just delete them
+# from the dataset.
+insurance <- insurance[-which(is.na(insurance$policy_no)), ]
 
-policies[c(164824, 165589),]
-# For this case the changes are in the issue date, the zip_code, borough and neighborhood, 
-# premium and deductable.
-policies[c(187826, 187827),]
-# This time the second observation seems to be the same but for some columns. The second register
-# looks to have a badly codified date for the colum drv_dob. Also we can see changes in the
-# zip_code  column, the product and the premium.
+# Let's now explore each variable:
 
-# Since there are badly codified columns in both cases and keeping this rows would add complexity
-# for merging both datasets, let's just don't use them, so we must remove the corresponding 
-# policy numbers in the claims dataset for this observations before making a merge.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
